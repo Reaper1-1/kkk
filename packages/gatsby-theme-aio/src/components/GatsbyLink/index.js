0d9cc40767a7fc83ddec4519cd24bd25a965a595
Reaper1-1 <160847856+Reaper1-1@users.noreply.github.com>
2025-01-28 16:52:53 -0600

    Initialize dependabot/npm_and_yarn/cypress-5.6.0

---
+++ packages/gatsby-theme-aio/src/components/GatsbyLink/index.js
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import React, { forwardRef, useContext } from 'react';
+import { Link, withPrefix } from 'gatsby';
+import { isInternalLink } from '../../utils';
+import PropTypes from 'prop-types';
+import Context from '../Context';
+
+const GatsbyLink = forwardRef(({ to, ...props }, ref) => {
+  const { location, allSitePage, pathPrefix } = useContext(Context);
+  const pages = allSitePage.nodes.map((page) => withPrefix(page.path));
+
+  if (isInternalLink(to, location, pages)) {
+    return <Link to={to.replace(pathPrefix, '')} ref={ref} {...props} />;
+  }
+
+  return <a href={to} ref={ref} {...props} />;
+});
+
+GatsbyLink.propTypes = {
+  to: PropTypes.string
+};
+
+export { GatsbyLink };
---
+++ packages/gatsby-theme-aio/src/components/Resources/index.js
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import React from 'react';
+import { css } from '@emotion/core';
+import '@spectrum-css/typography';
+import PropTypes from 'prop-types';
+import { layoutColumns } from '../../utils';
+import { View } from '@adobe/react-spectrum';
+import { Flex } from '@adobe/react-spectrum';
+import LinkOut from '@spectrum-icons/workflow/LinkOut';
+import { getElementChild, isExternalLink, LARGE_SCREEN_WIDTH } from '../../utils';
+
+const Resources = ({ heading, links }) => {
+  return (
+    <aside
+      css={css`
+        width: ${layoutColumns(3)};
+        margin-left: var(--spectrum-global-dimension-size-400);
+        margin-top: var(--spectrum-global-dimension-size-400);
+
+        @media screen and (max-width: ${LARGE_SCREEN_WIDTH}) {
+          display: none;
+        }
+      `}>
+      {heading}
+      <ul
+        className="spectrum-Body--M"
+        css={css`
+          list-style: none;
+          padding: 0;
+        `}>
+        {React.Children.map(links.props.children, (item, i) => {
+          const link = getElementChild(item);
+          const href = link.props.href;
+
+          return (
+            <li
+              key={i}
+              css={css`
+                margin-top: var(--spectrum-global-dimension-size-200);
+              `}>
+              <Flex>
+                {link}
+                <View marginStart="size-100">{isExternalLink(href) && <LinkOut size="XS" />}</View>
+              </Flex>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
+};
+
+Resources.propTypes = {
+  heading: PropTypes.element,
+  links: PropTypes.element
+};
+
+export { Resources };
