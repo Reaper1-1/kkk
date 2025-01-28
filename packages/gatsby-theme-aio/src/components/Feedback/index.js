de11593e6ab2e61f84e4bfb21503d61a44b97dc2
Reaper1-1 <160847856+Reaper1-1@users.noreply.github.com>
2025-01-28 16:53:06 -0600

    Initialize dependabot/npm_and_yarn/gatsby-plugin-mdx-1.5.0

---
+++ packages/gatsby-theme-aio/src/components/Feedback/index.js
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import React from 'react';
+import { css } from '@emotion/core';
+import { Flex } from '@adobe/react-spectrum';
+import { View } from '@adobe/react-spectrum';
+import { Button } from '@adobe/react-spectrum';
+import { DialogTrigger } from '@adobe/react-spectrum';
+import { AlertDialog } from '@adobe/react-spectrum';
+
+const FeedbackButton = ({ variant }) => (
+  <DialogTrigger>
+    <Button UNSAFE_className={`feedback-${variant.toLowerCase()}`} variant="primary">
+      {variant}
+    </Button>
+    <AlertDialog title="Thank you for your feedback" primaryActionLabel="Close">
+      Thank you for helping improve Adobe's documentation.
+    </AlertDialog>
+  </DialogTrigger>
+);
+
+export const Feedback = () => (
+  <Flex alignItems="center">
+    <span
+      css={css`
+        padding-right: var(--spectrum-global-dimension-size-200);
+      `}>
+      Was this helpful?
+    </span>
+    <Flex>
+      <View marginEnd="size-100">
+        <FeedbackButton variant="Yes" />
+      </View>
+      <View>
+        <FeedbackButton variant="No" />
+      </View>
+    </Flex>
+  </Flex>
+);
---
+++ packages/gatsby-theme-aio/src/components/NextPrev/index.js
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link as GatsbyLink } from 'gatsby';
+import '@spectrum-css/typography';
+import { View } from '@adobe/react-spectrum';
+import { Flex } from '@adobe/react-spectrum';
+import { Link } from '@adobe/react-spectrum';
+import ChevronLeft from '@spectrum-icons/workflow/ChevronLeft';
+import ChevronRight from '@spectrum-icons/workflow/ChevronRight';
+
+const NextPrev = ({ nextPage, previousPage }) =>
+  nextPage || previousPage ? (
+    <div className="spectrum-Body--M">
+      <Flex marginBottom="size-800" marginTop="size-800" gap="size-200">
+        <View>
+          {previousPage && (
+            <Link isQuiet={true}>
+              <GatsbyLink to={previousPage.path} rel="prev">
+                <Flex alignItems="center">
+                  <ChevronLeft size="S" />
+                  <View marginStart="size-50">{previousPage.title}</View>
+                </Flex>
+              </GatsbyLink>
+            </Link>
+          )}
+        </View>
+        <View marginStart="auto">
+          {nextPage && (
+            <Link isQuiet={true}>
+              <GatsbyLink to={nextPage.path} rel="next">
+                <Flex alignItems="center">
+                  <View marginEnd="size-50">{nextPage.title}</View>
+                  <ChevronRight size="S" />
+                </Flex>
+              </GatsbyLink>
+            </Link>
+          )}
+        </View>
+      </Flex>
+    </div>
+  ) : null;
+
+NextPrev.propTypes = {
+  nextPage: PropTypes.object,
+  previousPage: PropTypes.object
+};
+
+export { NextPrev };
